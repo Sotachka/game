72ab110a17f42926d2712f306928e30d8045bcb4
sotachka <tualkhsot@gmail.com>
2026-02-07 23:58:45 +0600

    Создал класс Command и класс AttackCommand

---
+++ Project1/Command.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+class  Command
+{
+public:
+	~Command() {};
+	virtual void Execute() = 0;
+
+};
+
+class AttackCommand : public Command
+{
+public:
+	void Execute() override	
+	{
+		
+	}
+};
