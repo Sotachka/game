0556cb16fc34d2e3bff6c494606347ba3ff91901
Sotachka <tualkhsot@gmail.com>
2026-03-16 23:24:43 +0600

    Отделил реализацию в Command.cpp оставил объявление в Command.h

---
+++ Project1/Command.cpp
@@ -0,0 +1,59 @@
+﻿#include "Command.h"
+#include "Player.h"
+#include "Entity.h"
+#include "Creature.h"
+#include "Data.h"
+#include "Map"
+void SleepCommand::Execute(Player* p, Entity* (&grid)[30][30])
+{
+	p->hp = 100;
+	std::cout << "Ваше здоровье восстановлено." << std::endl;
+}
+
+int AttackCommand::CalculateDamage(int dmg, int crit, const Item* armor)
+{
+	int takenDamage = 0;
+	int critChance = rand() % 100;
+	if (armor != nullptr)
+	{
+		if (dmg > armor->defense)
+		{
+			takenDamage = dmg - armor->defense;
+		}
+		else
+		{
+			takenDamage = 1;
+		}
+	}
+	else
+	{
+		takenDamage += dmg;
+	}
+	if (critChance < crit)
+	{
+		takenDamage += crit;
+		std::cout << "КРИТ!" << std::endl;
+	}
+	std::cout << "Было нанесено " << takenDamage << " урона" << std::endl;
+	return takenDamage;
+}
+void AttackCommand::Execute(Player* p, Entity* (&grid)[Map::HEIGHT][Map::WIDTH])
+{
+	
+	Creature* target = dynamic_cast<Creature*>(grid[p->row][p->col]);
+	target->hp -= CalculateDamage(p->weapon->damage, p->weapon->critical, target->armor);
+	if (target->hp <= 0)
+	{
+		std::cout << "Враг умер." << " Обыскав " << grid[p->row][p->col]->name << " вы нашли " << target->money << " денег" << std::endl;
+		p->money += target->money;
+		std::cout << "Теперь у вас " << p->money << " денег" << std::endl;
+		target->exist = false;
+	}
+	else
+	{
+		std::cout << "Теперь у врага " << target->hp << " здоровья" << std::endl;
+		std::cout << "Враг атакует в ответ" << std::endl;
+		p->hp -= CalculateDamage(target->weapon->damage, target->weapon->critical, p->armor);
+		std::cout << "Теперь у вас " << p->hp << " здоровья" << std::endl;
+	}
+}
--- Project1/Command.h
+++ Project1/Command.h
@@ -1,7 +1,8 @@
 ﻿#pragma once
-#include "Player.h"
-#include "Enemy.h"
 class Entity;
+class Player;
+class Enemy;
+class Item;
 class Command
 {
 public:	
@@ -12,67 +13,19 @@ class SleepCommand :public Command
 {
 public:
 	SleepCommand() {};
-	void Execute (Player*p, Entity*(&grid)[30][30])override
-	{
-		p->hp = 100;
-		std::cout << "Ваше здоровье восстановлено." << std::endl;
-	}
+	void Execute(Player* p, Entity* (&grid)[30][30])override;
+	
 };
 class BuyCommand : public Command 
 {
 public:
-	void Execute (Player*p, Entity* (&grid)[30][30])override
-	{
-		
-	}
+	void Execute(Player* p, Entity* (&grid)[30][30])override;
+	
 };
 class AttackCommand : public Command
 {
 public:
-	int CalculateDamage(int dmg, int crit, const Item *armor)
-	{
-		int takenDamage = 0;
-		int critChance = rand() % 100;
-		if (armor != nullptr)
-		{
-			if (dmg > armor->defense)
-			{
-				takenDamage = dmg - armor->defense;
-			}
-			else
-			{
-				takenDamage = 1;
-			}
-		}
-		else
-		{
-			takenDamage += dmg;
-		}
-		if (critChance < crit)
-		{
-			takenDamage += crit;
-			std::cout << "КРИТ!" << std::endl;
-		}
-		std::cout << "Было нанесено " << takenDamage << " урона" << std::endl;
-		return takenDamage;
-	}
-	void Execute (Player*p, Entity* (&grid)[Map::HEIGHT][Map::WIDTH])override
-	{	
-		Creature* target = dynamic_cast<Creature*>(grid[p->row][p->col]);
-		target->hp -= CalculateDamage(p->weapon->damage, p->weapon->critical, target->armor);
-		if (target->hp <= 0)
-		{
-			std::cout << "Враг умер." << " Обыскав " << grid[p->row][p->col]->name << " вы нашли " << target->money <<" денег" << std::endl;
-			p->money += target->money;
-			std::cout << "Теперь у вас " << p->money << " денег" << std::endl;
-			target->exist = false; 
-		}
-		else
-		{
-			std::cout << "Теперь у врага " << target->hp << " здоровья" << std::endl;
-			std::cout << "Враг атакует в ответ" << std::endl;
-			p->hp -= CalculateDamage(target->weapon->damage, target->weapon->critical, p->armor);                                          
-			std::cout << "Теперь у вас " << p->hp << " здоровья" << std::endl;
-		}
-	}
+	int CalculateDamage(int dmg, int crit, const Item* armor);
+	void Execute(Player* p, Entity* (&grid)[Map::HEIGHT][Map::WIDTH])override;
+	
 };
