5cf7eb60d3be2ffbf60d46d735df27cfb1728f6a
Sotachka <tualkhsot@gmail.com>
2026-04-02 23:39:39 +0600

    починил магазин

--- Project1/Command.cpp
+++ Project1/Command.cpp
@@ -4,6 +4,8 @@
 #include "Creature.h"
 #include "Data.h"
 #include "Map"
+#include "Object.h"
+class Shop;
 void SleepCommand::Execute(Player* p, Entity* (&grid)[30][30])
 {
 	p->hp = 100;
@@ -11,7 +13,35 @@ void SleepCommand::Execute(Player* p, Entity* (&grid)[30][30])
 }
 void BuyCommand::Execute(Player* p, Entity* (&grid)[30][30])
 {
-	std::cout << "BuyCommand" << std::endl;
+	Shop* target = dynamic_cast<Shop*>(grid[p->row][p->col]);
+	std::cout << "Какой предмет вы хотите приобрести?\nВведите номер предмета:" << std::endl;
+	int index; std::cin >> index;
+	if (index <= 5 && index > 0)
+	{
+		index -= 1;
+		if (target->stock[index] != nullptr)
+		{
+			if (p->money < target->stock[index]->cost)
+			{
+				std::cout << "Вам не хватает денег на это!" << std::endl;
+			}
+			else
+			{
+				p->money -= target->stock[index]->cost;
+				std::cout << "Вы купили: " << target->stock[index]->name << " ценой в " << target->stock[index]->cost << " денег" << std::endl;
+				if (target->stock[index]->damage > 0)
+				{
+					p->weapon = target->stock[index];
+				}
+				else if (target->stock[index]->defense > 0)
+				{
+					p->armor = target->stock[index];
+				}
+				target->stock[index] = nullptr;
+			}
+		}
+		else std::cout << "Этот предмет уже куплен!" << std::endl;
+	}	
 }
 int AttackCommand::CalculateDamage(int dmg, int crit, const Item* armor)
 {
